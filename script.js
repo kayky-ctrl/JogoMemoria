63740dab63cdf5be6cdb8f10c293d42de4fe17cc
Kayky Rodrigues de Paula <49207297825@escola927.edu>
2025-09-30 17:12:03 -0300

    Jogo da memoria

---
+++ script.js
@@ -0,0 +1,5 @@
+let meuBotao = document.getElementById("meuBotao");
+
+meuBotao.addEventListener("click", function() {
+  alert("Parabéns! Você completou este jogo da memória. Que tal experimentar uma dificuldade diferente ou jogar novamente na mesma dificuldade?");
+});
